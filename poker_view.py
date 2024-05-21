908bac5fbf96cc2bfb878aae93f9ca88d7e5a8a5
Luca Pontellini <luca.pontellini@studenti.isissgobetti.it>
2024-05-21 11:25:21 +0200

    edit

--- poker_view.py
+++ poker_view.py
@@ -361,12 +361,12 @@ def clear_all_data():
                 data["remaining_money"] = 0
                 f.seek(0)
                 json.dump(data, f, indent=4)
-            return redirect(url_for('cashier_dashboard'))
+            return redirect(url_for('cashier_operation'))
         except json.decoder.JSONDecodeError as e:
             return f"Errore: Impossibile decodificare il file JSON - {str(e)}"
     else:
         create_or_update_data()
-        return redirect(url_for('cashier_dashboard'))
+        return redirect(url_for('cashier_operation'))
 
 @app.route('/cashier_dashboard', methods=['GET', 'POST'])
 def cashier_dashboard_page():
