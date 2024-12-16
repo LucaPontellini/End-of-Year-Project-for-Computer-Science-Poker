e643b71e07f47cd741e364f54b92552dba96430a
Angelo Galanti <angelo.galanti@isissgobetti.it>
2024-12-16 10:02:12 +0100

    correzione

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
+
+def get_random_card(suit):
+    return f"{suit}/{random.randint(1, 13):02d}_{suit}.png"
+
 @app.route('/')
 def index():
+    card_images_folder = 'foto_delle_carte'
+    suits = ['hearts', 'diamonds', 'clubs', 'spades']
 
-    # Assegnazione delle carte
-    user_cards = ["01_spades.png", "13_hearts.png"]  # L'utente ha l'asso di picche e il re di cuori
-    dealer_cards = ["05_clubs.png", "11_diamonds.png"]  # Il dealer ha il 5 di fiori e il fante di quadri
-    community_cards = ["07_diamonds.png", "08_spades.png", "09_hearts.png", "11_hearts.png", "12_diamonds.png"]  # Le carte comunitarie
-    
-    # Rendering del template HTML
+    user_cards = [f"{card_images_folder}/{get_random_card(random.choice(suits))}" for _ in range(2)]
+    dealer_cards = [f"{card_images_folder}/{get_random_card(random.choice(suits))}" for _ in range(2)]
+    community_cards = [f"{card_images_folder}/{get_random_card(random.choice(suits))}" for _ in range(5)]
+
     return render_template('poker.html', user_cards=user_cards, dealer_cards=dealer_cards, community_cards=community_cards)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True)
